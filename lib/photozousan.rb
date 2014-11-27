630da29b4a94873f813454e724ee8a5292114ac6
irasally <irasally@gmail.com>
2014-11-27 20:11:37 +0900

    Add bin files.

--- lib/photozousan.rb
+++ lib/photozousan.rb
@@ -3,7 +3,7 @@ require "photozousan/client"
 require 'io/console'
 
 module Photozousan
-  def download_all_images(album_id, limit=100)
+  def self.run(album_id, limit=100)
     puts 'your photozou id?:'
     id = gets.chop.to_s
     puts 'your photozou password?:'
