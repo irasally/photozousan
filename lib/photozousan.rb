fe7ccc0862d6d8b99a4428d9835464e548b0717b
irasally <irasally@gmail.com>
2014-11-28 18:14:33 +0900

    Fix chop to chomp

--- lib/photozousan.rb
+++ lib/photozousan.rb
@@ -9,9 +9,9 @@ module Photozousan
     print 'donwload image limit?(1-1000):'
     limit = gets.to_i
     print 'your photozou id?:'
-    id = gets.chop.to_s
+    id = gets.chomp
     print 'your photozou password?:'
-    pass = STDIN.noecho(&:gets).chop
+    pass = STDIN.noecho(&:gets).chomp
     Client.new(id, pass).dowmload_all_images(album_id, limit)
   end
 end
